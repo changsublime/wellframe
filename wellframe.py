6dd51bab062d5f16ef46725e8bfac51b3af8e010
changsublime <clim01@college.harvard.edu>
2018-10-12 02:13:31 -0400

    Add files via upload

---
+++ wellframe.py
@@ -0,0 +1,101 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+patients = [{"name" : "Changseob",
+            "medications" : []}]
+medications = [{"name" : "Xysol"}]
+
+class Patient(Resource):
+    def get(self, name):
+        for patient in patients:
+            if(name == patient["name"]):
+                return patient
+        return "Patient not found"
+
+    def post(self, name):
+        for patient in patients:
+            if (name == patient["name"]):
+                return "Patient {} already exists".format(name)
+
+        patient = {
+            "name" : name,
+            "medications" : []
+        }
+
+        patients.append(patient)
+        return patient
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("medications")
+        args = parser.parse_args()
+
+        available = 0
+
+        for med in medications:
+            if (args["medications"] == med["name"]):
+                available += 1
+
+        if (available == 0):
+            return "Medication {} is not available.".format(args["medications"])
+
+        for patient in patients:
+            if (name == patient["name"]):
+                if (args["medications"] in patient["medications"]):
+                    return "Patient {} already takes {}".format(name, args["medications"])
+                (patient["medications"]).append(args["medications"])
+                return patient
+
+        patient = {
+            "name": name,
+            "medications": [args["medications"]],
+        }
+
+        patients.append(patient)
+        return patient
+
+    def delete(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("medications")
+        args = parser.parse_args()
+
+        for patient in patients:
+            if (name == patient["name"]):
+                patient["medications"] = [med for med in patient["medications"] if args["medications"] != med]
+                return "Medication {} has been deleted from Patient {}'s list of medications".format(args["medications"], name)
+
+        return "Patient {} does not exist".format(name)
+
+
+class Medication(Resource):
+    def get(self, name):
+        for medication in medications:
+            if(name == medication["name"]):
+                return medication
+        return "Medication not found"
+
+    def post(self, name):
+        for medication in medications:
+            if (name == medication["name"]):
+                return "Medication {} already exists".format(name)
+
+        medication = {
+            "name": name
+        }
+
+        medications.append(medication)
+        return medication
+
+    def delete(self, name):
+        global medications
+        medications = [med for med in medications if name != med["name"]]
+        return "Medication {} has been deleted.".format(name)
+
+      
+api.add_resource(Patient, "/patient/<string:name>")
+api.add_resource(Medication, "/medication/<string:name>")
+
+app.run(debug=True)
